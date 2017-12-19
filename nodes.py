7d6bbdb4b58caf23ac09698e575af70f499d94e4
Eskaype <sk6407@nyu.edu>
2017-12-19 16:20:33 -0500

    Add files via upload

---
+++ nodes.py
@@ -0,0 +1,356 @@
+"""Module for nodes of factor graphs.
+
+This module contains classes for nodes of factor graphs,
+which are used to build factor graphs.
+
+Classes:
+    Node: Abstract class for nodes.
+    VNode: Class for variable nodes.
+    IOVNode: Class for custom input-output variable nodes.
+    FNode: Class for factor nodes.
+    IOFNode: Class for custom input-output factor nodes.
+
+"""
+
+from abc import ABCMeta, abstractmethod, abstractproperty
+from enum import Enum
+from types import MethodType
+
+import networkx as nx
+import numpy as np
+
+from . import rv
+
+
+class NodeType(Enum):
+
+    """Enumeration for node types."""
+
+    variable_node = 1
+    factor_node = 2
+
+
+class Node(metaclass=ABCMeta):
+
+    """Abstract base class for all nodes."""
+
+    def __init__(self, label):
+        """Create a node with an associated label."""
+        self.__label = str(label)
+        self.__graph = None
+
+    def __str__(self):
+        """Return string representation."""
+        return self.__label
+
+    @abstractproperty
+    def type(self):
+        pass
+
+    @property
+    def graph(self):
+        return self.__graph
+
+    @graph.setter
+    def graph(self, graph):
+        self.__graph = graph
+
+    def neighbors(self, exclusion=None):
+        """Get all neighbors with a given exclusion.
+
+        Return iterator over all neighboring nodes
+        without the given exclusion node.
+
+        Positional arguments:
+        exclusion -- the exclusion node
+
+        """
+
+        if exclusion is None:
+            return nx.all_neighbors(self.graph, self)
+        else:
+            # Build iterator set
+            iterator = (exclusion,) \
+                if not isinstance(exclusion, list) else exclusion
+
+            # Return neighbors excluding iterator set
+            return (n for n in nx.all_neighbors(self.graph, self)
+                    if n not in iterator)
+
+    @abstractmethod
+    def spa(self, tnode):
+        pass
+
+    @abstractmethod
+    def mpa(self, tnode):
+        pass
+
+    @abstractmethod
+    def msa(self, tnode):
+        pass
+
+    @abstractmethod
+    def mf(self, tnode):
+        pass
+
+
+class VNode(Node):
+
+    """Variable node.
+
+    Variable node inherited from node base class.
+    Extends the base class with message passing methods.
+
+    """
+
+    def __init__(self, label, init=None, observed=False):
+        """Create a variable node."""
+        super().__init__(label)
+        if init is not None:
+            self.init = init
+        else:
+            self.init = rv.Discrete(np.array([1, 1]), self)
+        self.observed = observed
+
+    @property
+    def type(self):
+        return NodeType.variable_node
+
+    @property
+    def init(self):
+        return self.__init
+
+    @init.setter
+    def init(self, init):
+        self.__init = init
+
+    def belief(self, normalize=True):
+        """Return belief of the variable node.
+
+        Args:
+            normalize: Boolean flag if belief should be normalized.
+
+        """
+        iterator = self.graph.neighbors(self)
+
+        # Pick first node
+        n = next(iterator)
+
+        # Product over all incoming messages
+        belief = self.graph[n][self]['object'].get_message(n, self)
+        for n in iterator:
+            belief *= self.graph[n][self]['object'].get_message(n, self)
+
+        if normalize:
+            belief = belief.normalize()
+
+        return belief
+
+    def maximum(self, normalize=True):
+        """Return the maximum probability of the variable node.
+
+        Args:
+            normalize: Boolean flag if belief should be normalized.
+
+        """
+        b = self.belief(normalize)
+        return np.amax(b.pmf)
+
+    def argmax(self):
+        """Return the argument for maximum probability of the variable node."""
+        # In case of multiple occurrences of the maximum values,
+        # the indices corresponding to the first occurrence are returned.
+        b = self.belief()
+        return b.argmax(self)
+
+    def spa(self, tnode):
+        """Return message of the sum-product algorithm."""
+        if self.observed:
+            return self.init
+        else:
+            # Initial message
+            msg = self.init
+
+            # Product over incoming messages
+            for n in self.neighbors(tnode):
+                msg *= self.graph[n][self]['object'].get_message(n, self)
+
+            return msg
+
+    def mpa(self, tnode):
+        """Return message of the max-product algorithm."""
+        return self.spa(tnode)
+
+    def msa(self, tnode):
+        """Return message of the max-sum algorithm."""
+        if self.observed:
+            return self.init
+        else:
+            # Initial message
+            msg = self.init
+            msg = msg.log()
+
+            # Sum over incoming messages
+            for n in self.neighbors(self.graph, self, tnode):
+                msg += self.graph[n][self]['object'].get_message(n, self)
+
+            return msg
+
+    def mf(self, tnode):
+        """Return message of the mean-field algorithm."""
+        if self.observed:
+            return self.init
+        else:
+            return self.belief(self.graph)
+
+
+class IOVNode(VNode):
+
+    """Input-output variable node.
+
+    Input-output variable node inherited from variable node class.
+    Overwrites all message passing methods of the base class
+    with a given callback function.
+
+    """
+
+    def __init__(self, label, init=None, observed=False, callback=None):
+        """Create an input-output variable node."""
+        super().__init__(label, init, observed)
+        if callback is not None:
+            self.set_callback(callback)
+
+    def set_callback(self, callback):
+        """Set callback function.
+
+        Add bounded methods to the class instance in order to overwrite
+        the existing message passing methods.
+
+        """
+        self.spa = MethodType(callback, self)
+        self.mpa = MethodType(callback, self)
+        self.msa = MethodType(callback, self)
+        self.mf = MethodType(callback, self)
+
+
+class FNode(Node):
+
+    """Factor node.
+
+    Factor node inherited from node base class.
+    Extends the base class with message passing methods.
+
+    """
+
+    def __init__(self, label, factor=None):
+        """Create a factor node."""
+        super().__init__(label)
+        self.factor = factor
+        self.record = {}
+
+    @property
+    def type(self):
+        return NodeType.factor_node
+
+    @property
+    def factor(self):
+        return self.__factor
+
+    @factor.setter
+    def factor(self, factor):
+        self.__factor = factor
+
+    def spa(self, tnode):
+        """Return message of the sum-product algorithm."""
+        # Initialize with local factor
+        msg = self.factor
+
+        # Product over incoming messages
+        for n in self.neighbors(tnode):
+            msg *= self.graph[n][self]['object'].get_message(n, self)
+
+        # Integration/Summation over incoming variables
+        for n in self.neighbors(tnode):
+            msg = msg.marginalize(n, normalize=False)
+
+        return msg
+
+    def mpa(self, tnode):
+        """Return message of the max-product algorithm."""
+        self.record[tnode] = {}
+
+        # Initialize with local factor
+        msg = self.factor
+
+        # Product over incoming messages
+        for n in self.neighbors(tnode):
+            msg *= self.graph[n][self]['object'].get_message(n, self)
+
+        # Maximization over incoming variables
+        for n in self.neighbors(tnode):
+            self.record[tnode][n] = msg.argmax(n)  # Record for back-tracking
+            msg = msg.maximize(n, normalize=False)
+
+        return msg
+
+    def msa(self, tnode):
+        """Return message of the max-sum algorithm."""
+        self.record[tnode] = {}
+
+        # Initialize with logarithm of local factor
+        msg = self.factor.log()
+
+        # Sum over incoming messages
+        for n in self.neighbors(self.graph, self, tnode):
+            msg += self.graph[n][self]['object'].get_message(n, self)
+
+        # Maximization over incoming variables
+        for n in self.neighbors(self.graph, self, tnode):
+            self.record[tnode][n] = msg.argmax(n)  # Record for back-tracking
+            msg = msg.max(n)
+
+        return msg
+
+    def mf(self, tnode):
+        """Return message of the mean-field algorithm."""
+        # Initialize with local factor
+        msg = self.factor
+
+#TODO:    # Product over incoming messages
+#         for n in self.neighbors(graph, self, tnode):
+#             msg *= graph[n][self]['object'].get_message(n, self)
+#
+#         # Integration/Summation over incoming variables
+#         for n in self.neighbors(graph, self, tnode):
+#             msg = msg.int(n)
+
+        return msg
+
+
+class IOFNode(FNode):
+
+    """Input-output factor node.
+
+    Input-output factor node inherited from factor node class.
+    Overwrites all message passing methods of the base class
+    with a given callback function.
+
+    """
+
+    def __init__(self, label, factor, callback=None):
+        """Create an input-output factor node."""
+        super().__init__(self, label, factor)
+        if callback is not None:
+            self.set_callback(callback)
+
+    def set_callback(self, callback):
+        """Set callback function.
+
+        Add bounded methods to the class instance in order to overwrite
+        the existing message passing methods.
+
+        """
+        self.spa = MethodType(callback, self)
+        self.mpa = MethodType(callback, self)
+        self.msa = MethodType(callback, self)
+        self.mf = MethodType(callback, self)
---
+++ rv.py
@@ -0,0 +1,512 @@
+"""Module for random variables.
+
+This module contains classes for random variables and exceptions.
+
+Classes:
+    ParameterException: Exception for invalid parameters.
+    Discrete: Class for discrete random variables.
+    Gaussian: Class for Gaussian random variables.
+
+"""
+
+import numpy as np
+
+
+class ParameterException(Exception):
+
+    """Exception for invalid parameters."""
+
+    pass
+
+
+class Discrete(object):
+
+    """Class for discrete random variables.
+
+    A discrete random variable is defined by a single- or multi-dimensional
+    probability mass function. In addition, each dimension of the probability
+    mass function has to be associated with a variable. The variable is
+    represented by a variable node of the comprehensive factor graph.
+
+    """
+
+    def __init__(self, raw_pmf, *args):
+        """Initialize a discrete random variable.
+
+        Create a new discrete random variable with the given probability
+        mass function over the given variable nodes.
+
+        Args:
+            raw_pmf: A Numpy array representing the probability mass function.
+                The probability mass function does not need to be normalized.
+            *args: Instances of the class VNode representing the variables of
+                the probability mass function. The number of the positional
+                arguments must match the number of dimensions of the Numpy
+                array.
+
+        Raises:
+            ParameterException: An error occurred initializing with invalid
+                parameters.
+
+        """
+        pmf = np.asarray(raw_pmf, dtype=np.float64)
+
+        # Set probability mass function
+        self._pmf = pmf
+
+        # Set variable nodes for dimensions
+        if np.ndim(pmf) != len(args):
+            raise ParameterException('Dimension mismatch.')
+        else:
+            self._dim = args
+
+    @property
+    def pmf(self):
+        return self._pmf
+
+    @property
+    def dim(self):
+        return self._dim
+
+    def __str__(self):
+        """Return string representation of the discrete random variable."""
+        return str(self.pmf)
+
+    def __add__(self, other):
+        """Add other to self and return the result.
+
+        Args:
+            other: Summand for the discrete random variable.
+
+        Returns:
+            A new discrete random variable representing the summation.
+
+        """
+        pmf = np.convolve(self.pmf, other.pmf, 'same')
+
+        return Discrete(pmf, self.dim)
+
+    def __sub__(self, other):
+        """Subtract other from self and return the result.
+
+        Args:
+            other: Subtrahend for the discrete random variable.
+
+        Returns:
+            A new discrete random variable representing the subtraction.
+
+        """
+        pmf = np.convolve(self.pmf[::-1], other.pmf, 'same')
+
+        return Discrete(pmf, self.dim)
+
+    def __mul__(self, other):
+        """Multiply other with self and return the result.
+
+        Args:
+            other: Multiplier for the discrete random variable.
+
+        Returns:
+            A new discrete random variable representing the multiplication.
+
+        """
+        # Verify dimensions of multiplicand and multiplier.
+        if len(self.dim) < len(other.dim):
+            self._expand(other.dim, other.pmf.shape)
+        elif len(self.dim) > len(other.dim):
+            other._expand(self.dim, self.pmf.shape)
+
+        pmf = self.pmf * other.pmf
+
+        return Discrete(pmf, *self.dim)
+
+    def __iadd__(self, other):
+        """Method for augmented addition.
+
+        Args:
+            other: Summand for the discrete random variable.
+
+        Returns:
+            A new discrete random variable representing the summation.
+
+        """
+        return self.__add__(other)
+
+    def __isub__(self, other):
+        """Method for augmented subtraction.
+
+        Args:
+            other: Subtrahend for the discrete random variable.
+
+        Returns:
+            A new discrete random variable representing the subtraction.
+
+        """
+        return self.__sub__(other)
+
+    def __imul__(self, other):
+        """Method for augmented multiplication.
+
+        Args:
+            other: Multiplier for the discrete random variable.
+
+        Returns:
+            A new discrete random variable representing the multiplication.
+
+        """
+        return self.__mul__(other)
+
+    def __eq__(self, other):
+        """Compare self with other and return the boolean result.
+
+        Two discrete random variables are equal only if the probability mass
+        functions are equal and the order of dimensions are equal.
+
+        """
+        return np.allclose(self.pmf, other.pmf) \
+            and self.dim == other.dim
+
+    def _expand(self, dims, states):
+        """Expand dimensions.
+
+        Expand the discrete random variable along the given new dimensions.
+
+        Args:
+            dims: List of discrete random variables.
+
+        """
+        reps = [1, ] * len(dims)
+
+        # Extract missing dimensions
+        diff = [i for i, d in enumerate(dims) if d not in self.dim]
+
+        # Expand missing dimensions
+        for d in diff:
+            self._pmf = np.expand_dims(self.pmf, axis=d)
+            reps[d] = states[d]
+
+        # Repeat missing dimensions
+        self._pmf = np.tile(self.pmf, reps)
+        self._dim = dims
+
+    def normalize(self):
+        """Normalize probability mass function."""
+        pmf = self.pmf / np.sum(self.pmf)
+        return Discrete(pmf, *self.dim)
+
+    def marginalize(self, *dims, normalize=True):
+        """Return the marginal for given dimensions.
+
+        The probability mass function of the discrete random variable
+        is marginalized along the given dimensions.
+
+        Args:
+            *dims: Instances of discrete random variables, which should be
+                marginalized out.
+            normalize: Boolean flag if probability mass function should be
+                normalized after marginalization.
+
+        Returns:
+            A new discrete random variable representing the marginal.
+
+        """
+        axis = tuple(idx for idx, d in enumerate(self.dim) if d in dims)
+        pmf = np.sum(self.pmf, axis)
+        if normalize:
+            pmf /= np.sum(pmf)
+
+        new_dims = tuple(d for d in self.dim if d not in dims)
+        return Discrete(pmf, *new_dims)
+
+    def maximize(self, *dims, normalize=True):
+        """Return the maximum for given dimensions.
+
+        The probability mass function of the discrete random variable
+        is maximized along the given dimensions.
+
+        Args:
+            *dims: Instances of discrete random variables, which should be
+                maximized out.
+            normalize: Boolean flag if probability mass function should be
+                normalized after marginalization.
+
+        Returns:
+            A new discrete random variable representing the maximum.
+
+        """
+        axis = tuple(idx for idx, d in enumerate(self.dim) if d in dims)
+        pmf = np.amax(self.pmf, axis)
+        if normalize:
+            pmf /= np.sum(pmf)
+
+        new_dims = tuple(d for d in self.dim if d not in dims)
+        return Discrete(pmf, *new_dims)
+
+    def argmax(self, dim=None):
+        """Return the dimension index of the maximum.
+
+        Args:
+            dim: An optional discrete random variable along a marginalization
+                should be performed and the maximum is searched over the
+                remaining dimensions. In the case of None, the maximum is
+                search along all dimensions.
+
+        Returns:
+            An integer representing the dimension of the maximum.
+
+        """
+        if dim is None:
+            return np.unravel_index(self.pmf.argmax(), self.pmf.shape)
+        m = self.marginalize(dim)
+        return np.argmax(m.pmf)
+
+    def log(self):
+        """Natural logarithm of the discrete random variable.
+
+        Returns:
+            A new discrete random variable with the natural logarithm of the
+            probablitiy mass function.
+
+        """
+        return Discrete(np.log(self.value), self.dim)
+
+
+class Gaussian(object):
+
+    """Class for Gaussian random variables.
+
+    A Gaussian random variable is defined by a mean vector and a covariance
+    matrix. In addition, each dimension of the mean vector and the covariance
+    matrix has to be associated with a variable. The variable is
+    represented by a variable node of the comprehensive factor graph.
+
+    """
+
+    def __init__(self, raw_mean, raw_cov, *args):
+        """Initialize a Gaussian random variable.
+
+        Create a new Gaussian random variable with the given mean vector and
+        the given covariance matrix over the given variable nodes.
+
+        Args:
+            raw_mean: A Numpy array representing the mean vector.
+            raw_cov: A Numpy array representing the covariance matrix.
+            *args: Instances of the class VNode representing the variables of
+                the mean vector and covariance matrix, respectively. The number
+                of the positional arguments must match the number of dimensions
+                of the Numpy arrays.
+
+        Raises:
+            ParameterException: An error occurred initializing with invalid
+                parameters.
+
+        """
+        if raw_mean is not None and raw_cov is not None:
+            mean = np.asarray(raw_mean, dtype=np.float64)
+            cov = np.asarray(raw_cov, dtype=np.float64)
+
+            # Set mean vector and covariance matrix
+            if mean.shape[0] != cov.shape[0]:
+                raise ParameterException('Dimension mismatch.')
+            else:
+                # Precision matrix
+                self._W = np.linalg.inv(np.asarray(cov))
+                # Precision-mean vector
+                self._Wm = np.dot(self._W, np.asarray(mean))
+
+            # Set variable nodes for dimensions
+            if cov.shape[0] != len(args):
+                raise ParameterException('Dimension mismatch.')
+            else:
+                self._dim = args
+
+        else:
+            self._dim = args
+
+    @classmethod
+    def inf_form(cls, raw_W, raw_Wm, *args):
+        """...
+
+        ...
+
+        """
+        g = Gaussian(None, None, *args)
+        g._W = np.asarray(raw_W, dtype=np.float64)
+        g._Wm = np.asarray(raw_Wm, dtype=np.float64)
+        return g
+
+    @property
+    def mean(self):
+        return np.dot(np.linalg.inv(self._W), self._Wm)
+
+    @property
+    def cov(self):
+        return np.linalg.inv(self._W)
+
+    @property
+    def dim(self):
+        return self._dim
+
+    def __str__(self):
+        """Return string representation of the Gaussian random variable."""
+        return "%s %s" % (self.mean, self.cov)
+
+    def __add__(self, other):
+        """Add other to self and return the result.
+
+        Args:
+            other: Summand for the Gaussian random variable.
+
+        Returns:
+            A new Gaussian random variable representing the summation.
+
+        """
+        return Gaussian(self.mean + other.mean,
+                        self.cov + other.cov,
+                        *self.dim)
+
+    def __sub__(self, other):
+        """Subtract other from self and return the result.
+
+        Args:
+            other: Subrahend for the Gaussian random variable.
+
+        Returns:
+            A new Gaussian random variable representing the subtraction.
+
+        """
+        return Gaussian(self.mean - other.mean,
+                        self.cov - other.cov,
+                        *self.dim)
+
+    def __mul__(self, other):
+        """Multiply other with self and return the result.
+
+        Args:
+            other: Multiplier for the Gaussian random variable.
+
+        Returns:
+            A new Gaussian random variable representing the multiplication.
+
+        """
+        W = self._W + other._W
+        Wm = self._Wm + other._Wm
+        return Gaussian.inf_form(W, Wm, *self.dim)
+
+    def __iadd__(self, other):
+        """Method for augmented addition.
+
+        Args:
+            other: Summand for the Gaussian random variable.
+
+        Returns:
+            A new Gaussian random variable representing the summation.
+
+        """
+        return self.__add__(other)
+
+    def __isub__(self, other):
+        """Method for augmented subtraction.
+
+        Args:
+            other: Subtrahend for the Gaussian random variable.
+
+        Returns:
+            A new Gaussian random variable representing the subtraction.
+
+        """
+        return self.__sub__(other)
+
+    def __imul__(self, other):
+        """Method for augmented multiplication.
+
+        Args:
+            other: Multiplier for the Gaussian random variable.
+
+        Returns:
+            A new Gaussian random variable representing the multiplication.
+
+        """
+        return self.__mul__(other)
+
+    def __eq__(self, other):
+        """Compare self with other and return the boolean result.
+
+        Two Gaussian random variables are equal only if the mean vectors and
+        the covariance matrices are equal and the order of dimensions are
+        equal.
+
+        """
+        return np.allclose(self._W, other._W) \
+            and np.allclose(self._Wm, other._Wm) \
+            and self.dim == other.dim
+
+    def marginalize(self, *dims):
+        """Return the marginal for given dimensions.
+
+        The probability density function of the Gaussian random variable
+        is marginalized along the given dimensions.
+
+        Args:
+            *dims: Instances of Gaussian random variables, which should be
+                marginalized out.
+
+        Returns:
+            A new Gaussian random variable representing the marginal.
+
+        """
+        axis = tuple(idx for idx, d in enumerate(self.dim) if d not in dims)
+        mean = self.mean[np.ix_(axis, [0])]
+        cov = self.cov[np.ix_(axis, axis)]
+
+        new_dims = tuple(d for d in self.dim if d not in dims)
+        return Gaussian(mean, cov, *new_dims)
+
+    def maximize(self, *dims):
+        """Return the maximum for given dimensions.
+
+        The probability density function of the Gaussian random variable
+        is maximized along the given dimensions.
+
+        Args:
+            *dims: Instances of Gaussian random variables, which should be
+                maximized out.
+
+        Returns:
+            A new Gaussian random variable representing the maximum.
+
+        """
+        axis = tuple(idx for idx, d in enumerate(self.dim) if d not in dims)
+        mean = self.mean[np.ix_(axis, [0])]
+        cov = self.cov[np.ix_(axis, axis)]
+
+        new_dims = tuple(d for d in self.dim if d not in dims)
+        return Gaussian(mean, cov, *new_dims)
+
+    def argmax(self, dim=None):
+        """Return the dimension index of the maximum.
+
+        Args:
+            dim: An optional Gaussian random variable along a marginalization
+                should be performed and the maximum is searched over the
+                remaining dimensions. In the case of None, the maximum is
+                search along all dimensions.
+
+        Returns:
+            An integer representing the dimension of the maximum.
+
+        """
+        if dim is None:
+            return self.mean
+        m = self.marginalize(dim)
+        return m.mean
+
+    def log(self):
+        """Natural logarithm of the Gaussian random variable.
+
+        Returns:
+            A new Gaussian random variable with the natural logarithm of the
+            probability density function.
+
+        """
+        raise NotImplementedError
